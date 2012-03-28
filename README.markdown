d24e47ec360eab60e5f2fca11746317853f3f7bc
JLS <neo.jls@gmail.com>
2012-03-28 19:58:31 -0400

    initial commit

---
+++ README.markdown
@@ -0,0 +1,11 @@
+# GeoNotes Manager: sample application
+
+This sample application allows registered users to add, remove, and edit notes which they can place on geographical map. Also allows to add tag words to the notes for filtering capabilities.
+
+Main open source technologies used:
+
+* Leaflet
+* Devise
+* jQuery-mobile
+* rGeo
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geonotes::Application.config.secret_token = '6349cbe772503d8cdb40846c0361bacd060fca9f1a5b27cc28a8072991fad81a73110310d278956f42f4d077f1906e3220687e1693fcd771da163744105f12b9'
