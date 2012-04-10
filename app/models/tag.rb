9aab300342d067b9a5c8de97ad24ed978ff009c0
JLS <neo.jls@gmail.com>
2012-04-10 13:53:22 -0400

    added annotations for tag model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: tags
+#
+#  id         :integer(4)      not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :notes, :uniq => true
  
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: tags
+#
+#  id         :integer(4)      not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 require 'spec_helper'
 
 describe Tag do
